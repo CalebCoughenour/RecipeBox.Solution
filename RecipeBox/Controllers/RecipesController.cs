aa506084b4e0e4cd454868b37a6aa56b21ed5408
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-31 16:09:28 -0700

    fill in most of recipe views/controller
    Co-authored-by: Cameron Ray <CamLRay@users.noreply.github.com>
    Co-authored-by: William Rodriguez <william@rodriguez1.com>

--- RecipeBox/Controllers/RecipesController.cs
+++ RecipeBox/Controllers/RecipesController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using RecipeBox.Models;
 using System.Collections.Generic;
+using System;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -11,7 +12,6 @@ using System.Security.Claims;
 
 namespace RecipeBox.Controllers
 {
-  [Authorize]
   public class RecipesController : Controller
   {
     private readonly RecipeBoxContext _db;
@@ -22,8 +22,15 @@ namespace RecipeBox.Controllers
       _userManager = userManager;
       _db = db;
     }
+  
+    public ActionResult Index ()
+    {
+      List<Recipe> model = _db.Recipes.ToList();
+      return View(model);
+    }
 
-    public async Task<ActionResult> Index ()
+    [Authorize]
+     public async Task<ActionResult> MyRecipes ()
     {
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync(userId);
@@ -33,17 +40,31 @@ namespace RecipeBox.Controllers
 
     public ActionResult Create()
     {
+      ViewBag.IngredientId = _db.Ingredients.ToList();
       return View();
     }
 
     [HttpPost]
-    public async Task<ActionResult> Create(Recipe recipe)
+    public async Task<ActionResult> Create(Recipe recipe, List<SelectListItem> Ingredients)
     {
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync(userId);
       recipe.User = currentUser;
       _db.Recipes.Add(recipe);
       _db.SaveChanges();
+
+      if(Ingredients.Count >0)
+      {
+        foreach(SelectListItem ingredient in Ingredients)
+        {
+          if(ingredient.Selected)
+          {
+            var thisIngredient = _db.Ingredients.FirstOrDefault(entry => entry.Name == ingredient.Value);
+            _db.RecipeIngredients.Add(new RecipeIngredient() { RecipeId = recipe.RecipeId, IngredientId = thisIngredient.IngredientId });
+          }
+        _db.SaveChanges();
+        }
+      }
       return RedirectToAction("Index");
     }
 
@@ -82,6 +103,24 @@ namespace RecipeBox.Controllers
       var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
       _db.Recipes.Remove(thisRecipe);
       _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddIngredients(int id)
+    {
+      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
+      ViewBag.IngredientId = new SelectList(_db.Ingredients, "IngredientId", "Name");
+      return View(thisRecipe);
+    }
+
+    [HttpPost]
+    public ActionResult AddIngredients(Recipe recipe, int IngredientId)
+    {
+      if (IngredientId != 0)
+      {
+        _db.RecipeIngredients.Add(new RecipeIngredient() { RecipeId = recipe.RecipeId, IngredientId = IngredientId });
+        _db.SaveChanges();
+      }
       return RedirectToAction("Index");
     }
 
